9166a16eda876575bf130782c7e50e40097b988e
vicente <adolfolezcanogmi.com@gmail.com>
2023-03-15 21:36:48 -0300

    Actualizacion Main

--- src/main/java/com/vicente/FundamentosJavaSE.java
+++ src/main/java/com/vicente/FundamentosJavaSE.java
@@ -11,6 +11,6 @@ package com.vicente;
 public class FundamentosJavaSE {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Vicente");
     }
 }
